dcbf488e7029984e061f01f47a954b028085826b
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-01-05 20:51:23 -0600

    added mediatr

--- TechNotes.Application/DependencyInjection.cs
+++ TechNotes.Application/DependencyInjection.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.Extensions.DependencyInjection;
 using TechNotes.Application.Notes;
 
@@ -8,6 +7,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
         // Register application services here
         services.AddScoped<INoteService, NotesService>();
 
