4da808ece9759ae8ccbe01adb5b0823a5740717a
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-01-23 23:13:51 -0600

    added generic type for result

--- TechNotes.Domain/Abstractions/Result.cs
+++ TechNotes.Domain/Abstractions/Result.cs
@@ -13,6 +13,18 @@ public class Result
     }
 
     public static Result Success() => new Result(true);
-
     public static Result Failure(string errorMessage) => new Result(false, errorMessage);
+    public static Result<T> Ok<T>(T? value) => new Result<T>(value, true, null);
+    public static Result<T> Fail<T>(string errorMessage) => new Result<T>(default, false, errorMessage);
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    protected internal Result(T? value, bool isSuccessful, string? errorMessage = null)
+        : base(isSuccessful, errorMessage)
+    {
+        Value = value;
+    }
 }
