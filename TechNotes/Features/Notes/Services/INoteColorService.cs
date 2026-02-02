f5cdabc75f828cb93340e2ab1d1a9a7dbcbed47f
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-02-01 22:26:58 -0600

    separated color login on a service

---
+++ TechNotes/Features/Notes/Services/INoteColorService.cs
@@ -0,0 +1,8 @@
+namespace TechNotes.Features.Notes.Services;
+
+public interface INoteColorService
+{
+    string GetNoteBackgroundColor(int noteId);
+    string GetNoteBorderColor(int noteId);
+    string GetNoteInlineStyle(int noteId);
+}
