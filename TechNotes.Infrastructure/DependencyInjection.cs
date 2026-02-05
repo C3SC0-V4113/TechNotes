605c9b75791fe49d3e829f93ae5906a2fa77f66a
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-02-04 21:49:13 -0600

    setting auth and identity in dependency injection

--- TechNotes.Infrastructure/DependencyInjection.cs
+++ TechNotes.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,13 @@
 
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TechNotes.Application.Authentication;
 using TechNotes.Domain.Notes;
+using TechNotes.Infrastructure.Authentication;
 using TechNotes.Infrastructure.Repositories;
 
 namespace TechNotes.Infrastructure;
@@ -16,8 +21,26 @@ public static class DependencyInjection
             b => b.MigrationsAssembly("TechNotes.Infrastructure")));
 
         services.AddScoped<INoteRepository, NoteRepository>();
+        AddAuthentication(services);
 
         return services;
+    }
+
+    private static void AddAuthentication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+        services.AddCascadingAuthenticationState();
+        services.AddAuthorization();
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultChallengeScheme = IdentityConstants.ExternalScheme;
+        }).AddIdentityCookies();
+        services.AddIdentityCore<User>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddSignInManager<SignInManager<User>>()
+        .AddDefaultTokenProviders();
     }
 
 }
