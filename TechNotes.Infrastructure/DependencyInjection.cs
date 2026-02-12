226b04fc5a15f3646b81a9749b1066e386dfa316
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-02-11 23:35:30 -0600

    added custom middleware for not authenticated views

--- TechNotes.Infrastructure/DependencyInjection.cs
+++ TechNotes.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
@@ -28,6 +29,7 @@ public static class DependencyInjection
 
     private static void AddAuthentication(this IServiceCollection services)
     {
+        services.AddSingleton<IAuthorizationMiddlewareResultHandler, BlazorAuthorizationMiddlewareResultHandler>();
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
         services.AddCascadingAuthenticationState();
