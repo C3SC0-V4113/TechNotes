0afcb7e90b6e10f7cb4e9b0a16db794ee467bc55
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-02-24 23:28:36 -0600

    reading author on edit page

--- TechNotes.Application/Notes/GetNoteById/GetNoteByIdQueryHandler.cs
+++ TechNotes.Application/Notes/GetNoteById/GetNoteByIdQueryHandler.cs
@@ -1,11 +1,14 @@
+using TechNotes.Domain.User;
 namespace TechNotes.Application.Notes.GetNoteById;
 
 public class GetNoteByIdQueryHandler : IQueryHandler<GetNoteByIdQuery, NoteResponse?>
 {
     private readonly INoteRepository _noteRepository;
-    public GetNoteByIdQueryHandler(INoteRepository noteRepository)
+    private readonly IUserRepository _userRepository;
+    public GetNoteByIdQueryHandler(INoteRepository noteRepository, IUserRepository userRepository)
     {
         _noteRepository = noteRepository;
+        _userRepository = userRepository;
     }
     public async Task<Result<NoteResponse?>> Handle(GetNoteByIdQuery request, CancellationToken cancellationToken)
     {
@@ -14,6 +17,16 @@ public class GetNoteByIdQueryHandler : IQueryHandler<GetNoteByIdQuery, NoteRespo
         {
             return Result.Fail<NoteResponse?>("Note not found.");
         }
-        return note.Adapt<NoteResponse>();
+        var noteResponse = note.Adapt<NoteResponse>();
+        if (note.UserId != null)
+        {
+            var user = await _userRepository.GetUserByIdAsync(note.UserId);
+            noteResponse.UserName = user?.UserName ?? "Unknown User";
+        }
+        else
+        {
+            noteResponse.UserName = "Unknown User";
+        }
+        return noteResponse;
     }
 }
--- TechNotes/Features/Notes/NoteModel.cs
+++ TechNotes/Features/Notes/NoteModel.cs
@@ -7,4 +7,5 @@ public class NoteModel
     public string? Content { get; set; }
     public DateTime? PublishedAt { get; set; }
     public bool IsPublished { get; set; } = false;
+    public string? UserName { get; set; }
 }
