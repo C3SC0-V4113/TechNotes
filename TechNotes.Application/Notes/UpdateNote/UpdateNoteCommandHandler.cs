e812cf9303faf4d15eaa353771fdcf4d5754e215
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-03-02 23:13:49 -0600

    edit error if you don have permission

--- TechNotes.Application/Notes/UpdateNote/UpdateNoteCommandHandler.cs
+++ TechNotes.Application/Notes/UpdateNote/UpdateNoteCommandHandler.cs
@@ -1,17 +1,26 @@
+using TechNotes.Application.Users;
+
 namespace TechNotes.Application.Notes.UpdateNote;
 
 public class UpdateNoteCommandHandler : ICommandHandler<UpdateNoteCommand, NoteResponse?>
 {
     private readonly INoteRepository _noteRepository;
+    private readonly IUserService _userService;
 
-    public UpdateNoteCommandHandler(INoteRepository noteRepository)
+    public UpdateNoteCommandHandler(INoteRepository noteRepository, IUserService userService)
     {
         _noteRepository = noteRepository;
+        _userService = userService;
     }
 
     public async Task<Result<NoteResponse?>> Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
     {
         var existingNote = request.Adapt<Note>();
+        var currentUserCanEdit = await _userService.CurrentUserCanEditNoteAsync(existingNote.Id);
+        if (!currentUserCanEdit)
+        {
+            return Result.Fail<NoteResponse?>("You do not have permission to edit this note.");
+        }
         var updatedNote = await _noteRepository.UpdateNoteAsync(existingNote);
         if (updatedNote == null)
         {
