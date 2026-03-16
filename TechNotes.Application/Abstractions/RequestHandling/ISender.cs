3f3a3e38469e3d78967e82ec958b7623d6a83cd9
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2026-03-15 19:07:37 -0600

    starting isender implementation

---
+++ TechNotes.Application/Abstractions/RequestHandling/ISender.cs
@@ -0,0 +1,6 @@
+namespace TechNotes.Application.Abstractions.RequestHandling;
+
+public interface ISender
+{
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+}
